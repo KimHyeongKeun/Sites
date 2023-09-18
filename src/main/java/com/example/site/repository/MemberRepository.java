3f918dc15c9c7e92d3e6f1b3bdca44aec10e9d4c
KimHyeongKeun <philip981217@mju.ac.kr>
2023-09-18 23:21:01 +0900

    repository

--- src/main/java/com/example/site/repository/MemberRepository.java
+++ src/main/java/com/example/site/repository/MemberRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 
 public interface MemberRepository extends JpaRepository<Member,Integer> {
+
+    Member findByUsernameAndPassword(String username, String password);
 }
