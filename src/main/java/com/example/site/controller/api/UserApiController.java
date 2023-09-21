cc14875569749085b3a42206f2ccfb2101cde913
KimHyeongKeun <philip981217@mju.ac.kr>
2023-09-21 15:23:07 +0900

    userapicontroller

--- src/main/java/com/example/site/controller/api/UserApiController.java
+++ src/main/java/com/example/site/controller/api/UserApiController.java
@@ -1,68 +1,46 @@
 package com.example.site.controller.api;
 
 
-//import com.example.site.model.User;
+
+import com.example.site.dto.ResponseDto;
 import com.example.site.model.Member;
-import com.example.site.model.RoleType;
-import com.example.site.repository.MemberRepository;
 import com.example.site.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class UserApiController {
 
-    @Autowired  //type으로 의존성 주입(변수, 생성자, Setter메소드, 일반 메소드에서 적용 가능)
+    @Autowired
     private UserService userService;
 
-    @Autowired
-    private MemberRepository memberRepository;
+//    @Autowired
+//    private HttpSession session;
 
 
     @PostMapping("/api/user")
-    public String save(@RequestBody Member member){
+    public ResponseDto<Integer> save(@RequestBody Member member){   //username, password, email
         System.out.println("UserApiController: save호출됨");
-//        System.out.println("username"+username);
-        System.out.println("");
-        System.out.println("username: "+member.getUsername() + "email: "+member.getEmail() + "password: "+member.getPassword());
+        //실제 DB에 insert를 하고 아래에서 return이 되면된다.
 
-        member.setRole(RoleType.MEMBER);
-        userService.SignUp(member);
-
-        return "UserApiController: save호출됨";
+//        member.setRole(RoleType.USER);
+         userService.SignUp(member);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(),1); //
     }
 
-//    @PostMapping("/api/test")
-//    public String join(@RequestBody String username, String password, String email){
-////        System.out.println("username"+ username);
-////        System.out.println("password"+ password);
-////        System.out.println("email"+ email);
+//    @PostMapping("/api/user/login")
+//    public ResponseDto<Integer> login(@RequestBody Member member, HttpSession session){
+//        System.out.println("UserApiContoller: login호출됨");
+////        Member principal = userService.SignIn(member);     //principal(접근주체)
 //
-//        return "회원가입이 완료되었습니다.";
+//        if(principal!=null){
+//            session.setAttribute("principal", principal);
+//        }
+//        return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
 //    }
-
-//    @PostMapping("/api/posttest")
-//    public String join(@RequestBody Member member){
-//        return "post 요청"+member.getUsername()+" "+ member.getPassword();
-//    }
-
-    @GetMapping("/api/gettest")
-    public String getTest(Member member){
-
-        return "테스트 완료"+member.getUsername()+" "+ member.getPassword();
-    }
-
-
-    @PostMapping("/Dummy/join")
-    public String join(Member member){
-
-        System.out.println("Username: "+member.getUsername());
-        System.out.println("Password: "+member.getPassword());
-        System.out.println("Email: "+member.getEmail());
-
-        return "회원가입이 완료되었습니다.";
-
-    }
-
-
 }
